45c4544da767cd02557547ed979a9c7d7b764d0b
alexandrzhyliuk <alexzhyliuk3@gmail.com>
2025-09-05 20:53:05 +0300

    popup

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,7 @@ import * as tabs from './modules/tabs.js'
 import * as hero from './modules/hero.js'
 import * as reviews from './modules/reviews.js'
 import * as solutions from './modules/solutions.js'
+import * as popup from './modules/popup.js'
 
 // flsFunctions.isWebp()
 
